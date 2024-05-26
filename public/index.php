fae19dece15d1e951ee037ec659f90a90bc502de
tiranosaur <tiranosaur88@gmail.com>
2024-05-26 15:36:46 +0300

    fix: Deprecated: ini_set() :bug:

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+set_error_handler(function($errno, $errstr) {
+    return strpos($errstr, 'ini_set()') === 0;
+}, E_DEPRECATED);
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
